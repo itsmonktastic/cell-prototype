ad1506eefd5ce2861d56d806b64fe33d8d835f0b
Connor Stack <sirconnorstack@gmail.com>
2017-02-19 15:00:03 -0800

    Fill world.

--- main.rb
+++ main.rb
@@ -1,15 +1,49 @@
 #!/usr/bin/env ruby
-
 class Command
   SPLIT = 'SPLIT'
   SEND_SIGNAL = 'SEND_SIGNAL'
   SENSE_SIGNAL = 'SENSE_SIGNAL'
+  SENSE_CELL = 'SENSE_CELL'
   JUMP_IF_EQUAL = 'JUMP_IF_EQUAL'
 
-  attr_accessor :type
+  class Direction
+    attr_accessor :string_name
+    def initialize(string_name)
+      self.string_name = string_name
+    end
+    def to_s
+      self.string_name
+    end
+  end
+  UP = Direction.new('UP')
+  DOWN = Direction.new('DOWN')
+  LEFT = Direction.new('LEFT')
+  RIGHT = Direction.new('RIGHT')
 
-  def initialize(type)
+  PARAMETERS = {
+    SPLIT => [Direction],
+    SEND_SIGNAL => [Direction],
+    SENSE_SIGNAL => [Direction],
+    SENSE_CELL => [Direction],
+    JUMP_IF_EQUAL => [Fixnum],
+  }
+
+  attr_accessor :type, :parameters
+
+  def initialize(type, parameters=[])
     self.type = type
+    
+    if parameters.length != PARAMETERS[type].length
+      raise "Expected #{PARAMETERS[type].length} parameters, got #{parameters.length}"
+    end
+
+    PARAMETERS[type].each_with_index do |param_type, i|
+      unless parameters[i].kind_of?(param_type)
+        raise "Expected a #{param_type} but got a #{parameters[i].class} at position #{i}"
+      end
+    end
+
+    self.parameters = parameters
   end
 
   def to_s
@@ -18,11 +52,12 @@ class Command
 end
 
 class Cell
-  attr_accessor :commands, :id, :program_counter
+  attr_accessor :commands, :id, :program_counter, :is_new
 
   def initialize()
     self.commands = []
     self.program_counter = 0
+    self.is_new = true
   end
 
   def pretty_id
@@ -45,6 +80,9 @@ class World
   end
 
   def add_cell(cell, row, col)
+    return if row < 0 || row >= grid.length
+    return if col < 0 || col >= grid[0].length
+
     current_contents = self.grid[row][col]
     return unless current_contents.nil?
 
@@ -78,16 +116,23 @@ end
 def print_cell(cell)
   printf("%s :\n", cell.pretty_id)
   cell.commands.each_with_index do |command, i|
-    print "  "
+    if cell.program_counter == i
+      print "-> "
+    else
+      print "   "
+    end
     print command.to_s
-    print " <-" if cell.program_counter == i
+    if command.parameters.length > 0
+      print "(#{command.parameters.join(", ")})"
+    end
+
     print "\n"
   end
 end
 
 def simulate_cell_cycle(world, row, col)
   cell = world.grid[row][col]
-  return if cell.nil?
+  return if cell.nil? || cell.is_new
   command = cell.commands[cell.program_counter]
   cell.program_counter += 1
   cell.program_counter %= cell.commands.length
@@ -96,7 +141,18 @@ def simulate_cell_cycle(world, row, col)
   when Command::SPLIT
     new_cell = Cell.new
     new_cell.commands = cell.commands.dup
-    world.add_cell(new_cell, row-1, col)
+    case command.parameters[0]
+    when Command::UP
+      world.add_cell(new_cell, row-1, col)
+    when Command::RIGHT
+      world.add_cell(new_cell, row, col+1)
+    when Command::DOWN
+      world.add_cell(new_cell, row+1, col)
+    when Command::LEFT
+      world.add_cell(new_cell, row, col-1)
+    else
+      raise "unkown direction #{command.parameters}"
+    end
   else
     raise "unknown command #{command.type}"
   end
@@ -108,13 +164,24 @@ def simulate_world_cycle(world)
       simulate_cell_cycle(world, row, col)
     end
   end
+  world.grid.each do |cells|
+    cells.each do |cell|
+      unless cell.nil?
+        cell.is_new = false
+      end 
+    end
+  end
 end
 
 zygote = Cell.new()
-zygote.commands << Command.new(Command::SPLIT)
+zygote.commands << Command.new(Command::SPLIT, [Command::UP])
+zygote.commands << Command.new(Command::SPLIT, [Command::RIGHT])
+zygote.commands << Command.new(Command::SPLIT, [Command::DOWN])
+zygote.commands << Command.new(Command::SPLIT, [Command::LEFT])
+zygote.is_new = false
 world = World.new(zygote: zygote)
 
-2.times do
+10.times do
   print_world(world)
   simulate_world_cycle(world)
   print "\n"
